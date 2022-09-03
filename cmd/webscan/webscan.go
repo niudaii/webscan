0fde541ac221edf1ad1b96807c4ca83ad2edced3
niudaii <243536998@qq.com>
2022-09-03 11:58:11 +0800

    refactor: README

--- cmd/webscan/webscan.go
+++ cmd/webscan/webscan.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/niudaii/webscan/internal/runner"
 	"github.com/projectdiscovery/gologger"
+	"time"
 )
 
 func main() {
@@ -11,5 +12,8 @@ func main() {
 	if err != nil {
 		gologger.Fatal().Msgf("Could not create runner: %v", err)
 	}
+	start := time.Now()
+	gologger.Info().Msgf("当前时间: %v", start.Format("2006-01-02 15:04:05"))
 	newRunner.Run()
+	gologger.Info().Msgf("运行时间: %v", time.Since(start))
 }
